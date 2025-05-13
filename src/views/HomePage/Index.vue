d49e5da884dd5087d41e6ed421d9f005ccfc1455
Andre <andre.ncardoso@hotmail.com>
2025-05-13 09:35:13 -0300

    feat: refactor project structure and integrate Tailwind CSS, Pinia, and Vue Router
    - Updated package.json to include Tailwind CSS, Pinia, and Vue Router dependencies.
    - Refactored App.vue to use <router-view> for routing.
    - Removed HelloWorld.vue component as it is no longer needed.
    - Enhanced main.ts to set up Pinia and Vue Router.
    - Created a new router configuration in src/router/index.ts.
    - Added a global store using Pinia in src/stores/GlobalStore.ts.
    - Replaced existing CSS with Tailwind CSS in src/style.css.
    - Created a new HomePage view in src/views/HomePage/Index.vue.
    - Added Tailwind CSS configuration in tailwind.config.ts.
    - Updated TypeScript configuration files for path aliases.
    - Enhanced Vite configuration to support SCSS and Tailwind CSS.

---
+++ src/views/HomePage/Index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { useGlobalStore } from "@/stores/GlobalStore";
+
+const globalStore = useGlobalStore();
+</script>
+
+<template>
+  <div class="text-[34px] test">HomePage</div>
+  <div class="test__test2" @click="globalStore.count += 1">
+    {{ globalStore.count }}
+  </div>
+  <q-btn label="Click" color="primary" />
+</template>
+
+<style lang="scss">
+.test {
+  color: red;
+
+  &__test2 {
+    color: blue;
+  }
+}
+</style>
