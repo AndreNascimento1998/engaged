ae74530007b750a1dbe63146eefa69ce2443a87f
Andre <andre.ncardoso@hotmail.com>
2025-05-14 17:54:43 -0300

    feat: add Button component with click event and label prop

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const emit = defineEmits(["click"]);
+
+defineProps<{
+  label?: string;
+}>();
+</script>
+
+<template>
+  <q-btn @click="emit('click')" color="primary" :label="label" />
+</template>
